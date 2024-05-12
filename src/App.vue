5207f2b91c9b1254903b57b1c5604e6fefac7cc1
Dmitry-Klyuev <i6169696@gmail.com>
2024-05-12 10:53:16 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MyHello from '@/components/MyHello.vue'
+import Product from '@/components/Product.vue'
+</script>
+
+<template>
+
+<!--  <MyHello/>-->
+
+  <Product title="Nike" description="New design" :price='200'/>
+  <Product />
+  <Product/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  console.log(count)
+  count.value++
+}
+</script>
+
+<template>
+  <div class="flex size-1/2">
+    <h1 class="size-5 accent-amber-100">My name is Dmitry</h1>
+    <h2>i'm a frontend developer</h2>
+    <p>{{ count }}</p>
+    <button @click="increment">Click Me</button>
+    <input v-model="count" type="text" />
+  </div>
+</template>
+
+<style scoped></style>
