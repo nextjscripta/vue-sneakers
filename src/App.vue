2bd8fbbe149c11af90e7b65c43cf0692a9f476a4
Dmitry-Klyuev <i6169696@gmail.com>
2024-05-15 22:05:16 +0300

    add request to server and add sort items

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
+"id": 1
 <script setup>
 import Header from '@/components/Header.vue'
 import CardList from '@/components/CardList.vue'
 import Drawer from '@/components/Cart/Drawer.vue'
+import { onMounted, ref, watch } from 'vue'
+import axios from 'axios'
+
+const items = ref([])
+const sortBy = ref('')
+
+onMounted(async ()=>{
+    try {
+      const {data} = await axios.get('https://443bfca0a207c10b.mokky.dev/products')
+      items.value = data
+    }
+    catch (e) {
+      console.log(e)
+    }
+})
+watch(sortBy, async () => {
+  try {
+    const {data} = await axios.get(`https://443bfca0a207c10b.mokky.dev/products?sortBy=${sortBy.value}`)
+    items.value = data
+  }
+  catch (e) {
+    console.log(e)
+  }
+})
+const isSortedBy = (event) => {
+  sortBy.value = event.target.value
+}
+
 
 </script>
 
@@ -9,7 +38,6 @@ import Drawer from '@/components/Cart/Drawer.vue'
 <!--  <Drawer/>-->
   <div class="w-11/12 lg:w-3/4 m-auto bg-white rounded-xl mt-14 shadow-xl">
     <Header/>
-    <CardList/>
-
+    <CardList :items="items" :isSortedBy="isSortedBy"/>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,29 +1,42 @@
 <script setup>
 import Card from '@/components/Card.vue'
 
+defineProps({
+  items: Array,
+  isSortedBy: Function
+})
 </script>
 
 <template>
   <div class="flex flex-wrap gap-4 justify-center md:justify-between items-center pt-10 mx-10">
-    <h2 class="text-3xl font-bold ">Все кроссовки</h2>
+    <h2 class="text-3xl font-bold">Все кроссовки</h2>
     <div class="flex gap-5 items-center">
       <span>Сортировать</span>
-      <select class="border rounded-2xl py-3 px-7">
+      <select @change="isSortedBy" class="border rounded-2xl py-3 px-7">
         <option value="name">По названию</option>
-        <option value="des">По цене (дешевые)</option>
-        <option value="asc">По цене (доорогие)</option>
+        <option value="price">По цене (дешевые)</option>
+        <option value="-price">По цене (доорогие)</option>
       </select>
     </div>
     <div class="relative">
-      <img src="/search.svg" alt="search icon" class="absolute top-3.5 left-3">
-    <input type="text" placeholder="Поиск..." class="border border-slate-300 pl-8 pr-5 py-2 rounded-2xl">
+      <img src="/search.svg" alt="search icon" class="absolute top-3.5 left-3" />
+      <input
+        type="text"
+        placeholder="Поиск..."
+        class="border border-slate-300 pl-8 pr-5 py-2 rounded-2xl"
+      />
     </div>
   </div>
-  <div class="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-10 justify-center">
-    <Card title="Мужские Кроссовки" price="12 999" image="/sneakers/sneakers-1.jpg"/>
-    <Card title="Мужские Кроссовки" price="12 999" image="/sneakers/sneakers-2.jpg"/>
-    <Card title="Мужские Кроссовки" price="12 999" image="/sneakers/sneakers-3.jpg"/>
-    <Card title="Мужские Кроссовки" price="12 999" image="/sneakers/sneakers-4.jpg"/>
-    <Card title="Мужские Кроссовки" price="12 999" image="/sneakers/sneakers-5.jpg"/>
+  <div
+    class="grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-10 justify-center"
+  >
+    <Card
+    v-for="item in items"
+    :key="item.id"
+    :title="item.title"
+    :price="item.price"
+    :image="item.imageUrl"
+    />
+
   </div>
 </template>
